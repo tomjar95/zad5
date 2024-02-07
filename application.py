f9234061b8b543b98659e1865a88f23e91ba2593
TJ <tomaszjaronski95@gmail.com>
2024-02-07 21:25:45 +0100

    Init commit

---
+++ application.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+
+application = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    message = ''
+    if request.method == 'POST':
+        data = request.form['data']
+        message = f'Otrzymano dane: {data}'
+    return render_template('form.html', message=message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
